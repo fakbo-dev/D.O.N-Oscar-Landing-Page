f30759244295fd1e9c6c9cdb5733d82b536700d5
fakbo <fakbo0206@gmail.com>
2024-06-14 10:49:18 -0400

    steps done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             {/* Maybe need a change */}
             <section className="xl:bg-grid xl:bg-center xl:bg-repeat-y fixed top-0 bottom-0 left-0 right-0 z-10"></section>
             {/* Hero Part */}
-            <section className="h-[640px] xl:h-[840px] bg-hero bg-center lg:bg-cover bg-no-repeat bg-fixed xl:rounded-bl-[290px] relative  xl:rounded-tr-[290px] z-20">
+            <section className="h-[640px] xl:h-[840px] bg-hero bg-center lg:bg-cover bg-no-repeat bg-fixed xl:rounded-bl-[290px] relative  xl:rounded-br-[290px] z-20">
 
                 {/* Text */}
                 <div className="container mx-auto h-full flex items-center justify-center xl:justify-start">
--- components/Steps.jsx
+++ components/Steps.jsx
@@ -1,4 +1,6 @@
 import { FaHammer } from "react-icons/fa6";
+import { PiBlueprintLight } from "react-icons/pi";
+import { GrInspect } from "react-icons/gr";
 
 
 const Steps = () => {
@@ -8,12 +10,28 @@ const Steps = () => {
             <div className="container mx-auto">
 
                 <div className="grid grid-cols-1 gap-12 xl:grid-cols-3">
-                    <div className=" flex justify-center items-center flex-col">
-                        <div className="mb-5">
+                    <div className="text-center">
+                        <div className="mb-4 flex justify-center">
+                            <GrInspect className="text-accent text-5xl" />
+                        </div>
+                        <h3 className="h3 mb-5">Inspeccion</h3>
+                        <p className="mb-5 max-w-md mx-auto">Inspeccionamos su solicitud y sacamos un presupuesto a partir de su requerimiento </p>
+                    </div>
+
+                    <div className="text-center">
+                        <div className="mb-5 flex justify-center">
+                            <PiBlueprintLight className="text-accent text-5xl" />
+                        </div>
+                        <h3 className="h3 mb-5">planeacion</h3>
+                        <p className="mb-5 max-w-md mx-auto">en base al presupuesto nos encargamos de optimizar el precio para darles el mejor servicio asequible</p>
+                    </div>
+
+                    <div className="text-center">
+                        <div className="mb-5 flex justify-center">
                             <FaHammer className="text-accent text-5xl" />
                         </div>
-                        <h3 className="h3 mb-5">Planeacion del proyecto</h3>
-                        <p className="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, quam!</p>
+                        <h3 className="h3 mb-5">ejecucion</h3>
+                        <p className="mb-5 max-w-md mx-auto"> nos enfocamos en entregar un servicio excepcional al cliente. Nuestro equipo altamente capacitado se asegurará de que cada detalle se lleve a cabo con precisión. Estamos comprometidos con tu éxito y superar tus expectativas.</p>
                     </div>
                 </div>
             </div>
